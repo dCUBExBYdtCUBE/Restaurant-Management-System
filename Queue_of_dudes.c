18649c9fbc04125a8aaad5b254d412f233d08652
AyushAshok <112970378+AyushAshok@users.noreply.github.com>
2023-10-21 12:22:10 +0530

    Update Queue_of_dudes.c

--- Queue_of_dudes.c
+++ Queue_of_dudes.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<time.h>
 
 typedef struct p_details{
     char name[100];
     char phone[100];
     char email[100];
+    char time[30];
     int t_cost;
     struct p_details*next,*prev;
 }DETAILS;
@@ -30,6 +32,19 @@ DETAILS*create()
     return temp;
 }
 
+char*addtime()
+{
+    time_t rawtime;
+    struct tm *timeinfo;
+    static char buffer[80];
+
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", timeinfo);
+    return buffer;
+}
+
 DETAILS*insert_pd(QUEUE*det)
 {
     DETAILS*temp=create();
@@ -40,7 +55,8 @@ DETAILS*insert_pd(QUEUE*det)
     scanf("%10s",temp->phone);
     printf("Enter email: ");
     scanf("%99s",temp->email);
-    temp->t_cost=0; 
+    temp->t_cost=0;
+    strcpy(temp->time,addtime());
 
 
     if(det->rear==NULL && det->front==NULL)
@@ -57,6 +73,29 @@ DETAILS*insert_pd(QUEUE*det)
     return temp;  
 }
 
+DETAILS*delete_pd(QUEUE*det)
+{
+    DETAILS*l;
+    if(det->front==NULL)
+    {
+        printf("The Queue is empty\n");
+    }
+    else if(det->front==det->rear)
+    {
+        l=det->front;
+        det->front=NULL;
+        det->rear=NULL;
+    }
+    else
+    {
+        l=det->front;
+        det->front=det->front->next;
+        det->front->prev=NULL;
+        
+    }
+    return l;
+}
+
 
 
 void display(QUEUE*det)
@@ -70,10 +109,10 @@ void display(QUEUE*det)
     else
     {
         while(l!=det->rear){
-            printf("%s  %s  %s  %d\n",l->name,l->phone,l->email,l->t_cost);
+            printf("%s  %s  %s  %s  %d \n",l->name,l->phone,l->email,l->time,l->t_cost);
             l=l->next;
         }
-        printf("%s  %s  %s  %d\n",l->name,l->phone,l->email,l->t_cost);
+        printf("%s  %s  %s  %s  %d\n",l->name,l->phone,l->email,l->time,l->t_cost);
     }
 
 }
@@ -83,15 +122,23 @@ void display(QUEUE*det)
 int main()
 {
     QUEUE*det=malloc(sizeof(QUEUE));
+    DETAILS*l;
     init1(det);
     int n;
+    insert_pd(det);
     do
     {
-        insert_pd(det);
-        printf("Press 1 to add new dude and 0 to exit: ");
+        printf("Press 1 to add new dude, Press 2 to delete first dude and Press 3 to view the Queue and 0 to exit: ");
         scanf("%d",&n);
+        switch(n)
+        {
+        case 1:insert_pd(det);break;
+        case 2:l=delete_pd(det);
+                    printf("%s  %s  %s  %s  %d\n",l->name,l->phone,l->email,l->time,l->t_cost);
+                    free(l);break;
+        case 3:display(det);break;
+        }
     }while(n!=0);
-    display(det);
 }
 
 
